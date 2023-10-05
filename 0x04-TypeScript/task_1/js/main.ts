c6b3c7cf9b4d681df3b6ed9d854db06adf496f1b
lazesla <eslalazarus23@gmail.com>
2023-10-05 01:44:56 +0000

    creating teacher

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,11 @@
+/*interface Teacher {
+    firstName: string,
+    lastName: string,
+    location: string,
+    fullTimeEmployee: true | false,
+    yearsOfExperience?: number,
+    contract?: any
+}
+interface Directors extends Teacher {
+    numberOfReports: number
+}*/
