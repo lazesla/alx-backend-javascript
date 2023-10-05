98f6514e94b8285a58d7c18fd56b5b0ed6782fa1
lazesla <eslalazarus23@gmail.com>
2023-10-05 02:14:44 +0000

    creating directors and teachers

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,11 @@
+interface DirectorInterface {
+    workFromHome(): string,
+    getCoffeeBreak(): string,
+    workDirectorTasks(): string
+}
+
+interface TeacherInterface {
+    workFromHome(): string,
+    getCoffeeBreak(): string,
+    workTeacherTasks(): string
+}
